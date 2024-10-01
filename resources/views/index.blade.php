6a059912e35efde44ac278ea302dbd3f636f05fd
yuvrajdahal <iamyuvraj456@gmail.com>
2024-10-01 23:24:33 +0545

    feat:responsive pages

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,14 @@
 <x-layout>
     <!-- component -->
     <section>
-        <div class="py-16 min-h-screen">
-            <div class="container mx-auto  px-6 text-gray-500 flex gap-x-6 ">
-                <div >
-                    <div class="relative w-[750px] z-10 flex flex-col gap-3">
+        <div class="py-10 lg:py-16 min-h-screen">
+            <div
+                class="container mx-auto  px-6 text-gray-500 flex flex-col-reverse lg:flex-row gap-y-6 lg:gap-y-0 lg:gap-x-6 ">
+                <div>
+                    <div class="relative lg:w-[600px] xl:w-[750px] z-10 flex flex-col gap-3">
                         @foreach ($posts as $post)
                             <div class=" bg-white  border rounded-md px-4 py-4">
-                                <div class="flex justify-between">
+                                <div class="flex flex-col-reverse gap-y-4 lg:gap-y-0 lg:flex-row justify-between">
                                     <div class="flex flex-col justify-between">
                                         <div class="flex flex-col">
                                             <div class="flex items-center space-x-3">
@@ -40,7 +41,7 @@
                                         </div>
 
                                     </div>
-                                    <div class="w-[200px] h-[150px] overflow-hidden rounded-md">
+                                    <div class="h-[300px] lg:w-[200px] lg:h-[150px] overflow-hidden rounded-md">
                                         <img src='{{ Storage::url($post->image) }}' alt="post"
                                             class="h-full w-full hover:scale-110 transition-transform duration-300">
                                     </div>
@@ -54,7 +55,8 @@
                     <h1 class="font-semibold text-2xl mb-4">Featured Blogs</h1>
                     <div class="flex flex-wrap gap-2">
                         @foreach ($categories as $category)
-                        <button class="border px-4 py-2 text-sm  cursor-pointer rounded bg-gray-200 hover:bg-gray-300 active:bg-gray-200 ">{{$category->name}}</button>
+                            <button
+                                class="border px-4 py-2 text-sm  cursor-pointer rounded bg-gray-200 hover:bg-gray-300 active:bg-gray-200 ">{{ $category->name }}</button>
                         @endforeach
                     </div>
                 </div>
